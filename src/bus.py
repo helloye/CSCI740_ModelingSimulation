02d31a7d0e4c800bf4f7884da6fa37f93e7e9d7c
helloye <hello.paingy@gmail.com>
2019-12-04 19:10:58 -0500

    adding basic algo

--- src/bus.py
+++ src/bus.py
@@ -6,7 +6,27 @@ class Bus:
         # List of passengers currently on the bus
         self.passengers = []
 
-        # TODO: Determine if arrival/departure time is needed?
-
     def passenger_count(self):
         return len(self.passengers)
+
+    def queue_passenger(self, p):
+        self.passengers.append(p)
+
+    def print_queue(self):
+        for p in self.passengers:
+            print(p.passenger_info())
+
+    def move_to_next_station(self):
+        if self.location == 's1':
+            self.location = 's2'
+        elif self.location == 's2':
+            self.location = 's3'
+        elif self.location == 's3':
+            self.location = 's4'
+        elif self.location == 's4':
+            self.location = 's5'
+        elif self.location == 's5':
+            self.location = 's6'
+        else:
+            # Default safety case...
+            self.location = 's6'
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats import truncnorm
 from station import Station
+from bus import Bus
 from passenger import Passenger
 
 # 720 minute = 12 hrs.
@@ -37,7 +38,6 @@ def main():
     random_passengers_arrival.sort()
     # print(random_passengers_arrival)
 
-    # TODO: Determine if uniform arrival is ok?
     uniform_random_station = np.random.uniform(1, MAX_STATIONS+1, MAX_PASSENGERS_IN_SIMULATION)
     # print(uniform_random_station)
 
@@ -47,7 +47,6 @@ def main():
         pas_station_assignment = 's' + str(int(uniform_random_station[i]))
 
         # print("Queuing passenger to: " + pas_station_assignment + " | arrived at: " + str(pas_arrival_time))
-        # TODO: Determine uniform(?) destination of passenger (null destination for now/end of line)
 
         if pas_station_assignment == 's1':
             s1.queue_passenger(Passenger('s1', 'nullDestination', pas_arrival_time))
@@ -65,5 +64,46 @@ def main():
     # s3.print_queue()
     # s4.print_queue()
     # s5.print_queue()
+
+    # Init buses once system is retroactively set.
+    b1 = Bus('s1')
+
+    # Main algorithm - Repeat until end of sim/max time reached.
+    # 1) Update clock
+    # 2) Move buses (buses come from 5 to 715
+    # 3) Load passengers
+    # 4) Remove bus once it reaches the end and log for analysis.
+
+    # TESTING
+    while(s1.is_station_empty() is False):
+        print("S1-------")
+        s1.print_queue()
+        print("B1-------")
+        b1.print_queue()
+        print("-------\n\n\n")
+        b1.queue_passenger(s1.dequeue_passenger())
+
+    current_time = 0
+    buses = []
+    # While time is less than simulation time AND there are buses left in the system(?)
+    while(current_time < MAX_TIME_MINUTES):
+        # We will tick in intervals of 10 mins (constant travel time between stations)
+        # 1) Starting at 0, we will update the clock
+        current_time += 10
+        print("Current time: " + str(current_time))
+
+        # 2) Move buses:
+        # 2.1) Move buses to next station (this needs to be done first to make room for arrival)
+        # 2.2) Add bus to system current time is on the arrival interval
+
+        # 3) Load passengers
+        # 3.1) Determine passengers to load (arrival time is < current time, and there is a bus, and there is room on bus)
+        # 3.2) Determine passengers that waited too long and left (add to loss revenue)
+        # 3.3) For all passengers to be loaded, tag wait time (current time - arrival time)
+        # 3.4) Do the actual dequeue from station and queue to bus
+
+        # 4) Remove bus from system and log analytics
+        # 4.1) Buses that reached null station "s6" (?)
+        # 4.2) For every passenger in bus_passenger, we will log their wait time and other metrics (profit)?
 
 main()
